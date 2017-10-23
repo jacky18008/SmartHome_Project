fe561e90fba3f8c1c3d84f6962b240d8d8160387
jacky18008 <102701008@nccu.edu.tw>
2017-10-23 11:40:50 +0800

    client script, let client device to download

---
+++ client.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+# TCP client example
+
+#client_dir = "/Users/hsienhaochen/Documents/專題_張宏慶老師/Codes_2/Client"
+
+def device_check():
+    import time
+    print("checking devices......")
+    time.sleep(2)
+    
+    device_situation = "work normally"
+    
+    return device_situation
+
+
+
+#main function    
+if __name__ == "__main__":
+    import time
+    import socket,os
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    #use command line arguments to get port
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Self management of smart home.')
+    parser.add_argument('ip', type=str, help='ip to the server.')
+    parser.add_argument('port', type=str, help='port to the server.')
+    parser.add_argument('client_dir_path', type=str, help='dir_path of the client.')
+    parser.add_argument('server_dir_path', type=str, help='dir_path of the server.')
+
+    #port = int(input("client port?"))
+
+    #parse arguments
+    args = parser.parse_args()
+    ip = args.ip
+    port = int(args.port)
+    client_dir_path = args.client_dir_path
+    server_dir_path = args.server_dir_path
+    
+    #change work directory to client.
+    os.chdir(client_dir_path)
+    
+    
+
+    client_socket.connect((ip, port))
+
+    #check if get the port from python subprocess.
+    print("get port", port)
+
+    k = ' '
+    size = 1024
+
+
+
+    #get command from client
+    order_string = client_socket.recv(1024)
+    order_string = order_string.decode('UTF-8')
+
+    #new device, hasn't sent files
+    if order_string == "send files":
+
+        #find all json files and send
+
+        #list it first 
+        json_list = []
+        import os
+        for files in os.listdir(os.getcwd()):
+            if files.endswith(".json"):
+                json_list.append(files)
+        print("has to send ", len(json_list), "files......")
+
+        # send the number of files to server, for int had no encode, cast it to str.
+        client_socket.send(str(len(json_list)).encode('UTF-8'))
+
+        print("sending ", len(json_list), "files......")
+
+        for i in range( len(json_list) ):
+            file_name = json_list[i]
+            print("sending file's name: ", file_name)
+            client_socket.send(file_name.encode('UTF-8'))
+            with open(file_name, 'r') as fp:
+                #cont = fp.read()
+
+                while True:
+                    cont = fp.read(1024)
+                    if not cont:
+                        break
+                    while len(cont) > 0:
+                        intSent = client_socket.send(cont.encode('UTF-8'))
+                        cont = cont[intSent:]
+                fp.close()
+
+            #should sleep 3 seconds to avoid some error sending.
+            time.sleep(3)
+            client_socket.sendall('EOF'.encode('UTF-8'))
+
+        #get another command from client
+        order_string = client_socket.recv(1024)
+        order_string = order_string.decode('UTF-8')
+
+
+    if order_string == "check device":
+        device_situation = device_check()
+        client_socket.send(device_situation.encode('UTF-8'))
+        
+    #change work directory back to server.
+    os.chdir(server_dir_path)
+
+
+        
+        
+
+    
+    
+    
+    
+    
+    
+        
+   
+

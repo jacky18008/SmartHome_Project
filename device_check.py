880ddf2b8cda3a740c6d02be67d7382377506c4c
jacky18008 <102701008@nccu.edu.tw>
2017-12-16 15:44:41 +0800

    debug

--- device_check.py
+++ device_check.py
@@ -13,7 +13,7 @@ os.chdir(path)
 
 log_dir = os.path.join(os.getcwd(), "error_logs")
 #add log directory
-if not os.isdir(log_dir):
+if not os.path.isdir(log_dir):
     os.mkdir(log_dir)
 
 work_normally = np.random.randint(0, 10)
@@ -25,11 +25,12 @@ else:
     print("encounter some error")
     print("working on some checking and recovering...")
     
-    time.sleep(100)
+    time.sleep(30)
     
     # log the error message
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
     with open(os.path.join(log_dir, "error_log_"+current_time+".txt"), "w") as fp:
-        
+
+        print(os.path.join(log_dir, "error_log_"+current_time+".txt"))
         fp.write("error: "+current_time )
